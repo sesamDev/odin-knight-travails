dbb6d0380ae574a735e5de38d724d1a694b03a17
Sebastian <sebastian.samils@gmail.com>
2022-06-10 10:18:55 +0200

    rework done

--- lib/knigth_pawn.rb
+++ lib/knigth_pawn.rb
@@ -1,54 +1,64 @@
-board = [8, 8]
-count = 0
-board[0].times do |x|
-  board[1].times do |y|
-    p "#{x} #{y}"
-    count += 1
-  end
-end
-p count
+require 'pry-byebug'
+class Transform
+  attr_accessor :position, :possible_moves, :parent, :start_pos
 
-class KnightPawn
-  attr_accessor :y_moves, :x_moves
-
-  def initialize
-    @y_moves = {
-      forward: 2,
-      back: -2
-    }
-    @x_moves = {
-      left: -1,
-      right: 1
-    }
-  end
-
-  def knight_moves(origin, dest)
-    # code here
-  end
-
-  # Currently finds all possible moves from one position
-  def possible_moves(_board)
-    origin = [4, 0]
-    valid_movesXY = []
-    y_moves.each_key do |y|
-      x_moves.each_key do |x|
-        next if origin [0] + x_moves[x] > 7 || origin [0] + x_moves[x] < 0
-        next if origin[1] + y_moves[y] > 7 || origin[1] + y_moves[y] < 0
-
-        valid_movesXY << [origin [0] + x_moves[x], origin[1] + y_moves[y]]
-      end
-    end
-    y_moves.each_key do |y|
-      x_moves.each_key do |x|
-        next if origin [0] + y_moves[y] > 7 || origin [0] + y_moves[y] < 0
-        next if origin[1] + x_moves[x] > 7 || origin[1] + x_moves[x] < 0
-
-        valid_movesXY << [origin [0] + y_moves[y], origin[1] + x_moves[x]]
-      end
-    end
-    p valid_movesXY
+  def initialize(position, parent = nil, possible_moves = [])
+    @position = position
+    @possible_moves = possible_moves
+    @parent = parent
+    @start_pos = position
   end
 end
 
-knight = KnightPawn.new
-knight.possible_moves(board)
+class Knight_pawn
+  attr_accessor :transform
+
+  # Valid XY moves for a knight pawn
+  @@MOVES = [[1, 2], [2, 1], [-1, -2], [-2, -1], [1, -2], [-1, 2], [2, -1], [-2, 1]].freeze
+
+  # Return array of possible moves
+  def next_positions(current_position)
+    result = []
+    @@MOVES.each do |m|
+      next if current_position[0] + m[0] < 0 || current_position[0] + m[0] > 7
+      next if current_position[1] + m[1] < 0 || current_position[1] + m[1] > 7
+
+      result << [current_position[0] + m[0], current_position[1] + m[1]]
+    end
+    result
+  end
+
+  # Call to initiate "game"
+  def knight_moves(start_pos, target_pos)
+    queue = []
+    result = []
+    current = Transform.new(start_pos)
+    i = 0
+    until current.position == target_pos
+      current.possible_moves = next_positions(current.position)
+      current.possible_moves.each { |child| queue << Transform.new(child, current) }
+      current = queue.shift
+    end
+
+    history(current)
+  end
+
+  # Trace taken path and count number of steps taken.
+  def history(current)
+    # binding.pry
+    result = []
+    n_steps = 0
+    until current.parent.nil?
+      result << current.position
+      n_steps += 1
+      current = current.parent
+    end
+    result << current.start_pos
+    result.reverse!
+    puts "You made it in #{n_steps} Moves! Here\'s your path:"
+    result.each_index { |i| print "#{result[i]}\n" }
+  end
+end
+
+knight = Knight_pawn.new
+knight.knight_moves([3, 3], [4, 3])
